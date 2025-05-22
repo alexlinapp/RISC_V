0d11366ed2c49ec39beaf3c7734abc206990025a
Alex <alexlinapp@gmail.com>
2025-05-22 16:34:01 -0500

    updated gitignore, working on datapath

---
+++ single_cycle/src/datapath.sv
@@ -0,0 +1,17 @@
+module datapath
+    (
+        input   logic           clk, reset,
+        input   logic [1:0]     ResultSrc,
+        input   logic           PCSrc, ALUSrc,
+        input   logic           RegWrite,
+        input   logic [1:0]     ImmSrc,
+        input   logic [2:0]     ALUControl,
+        input   logic [31:0]    Instr,          
+        input   logic [31:0]    ReadData,
+        output  logic           Zero,
+        output  logic [31:0]    ALUResult, WriteData,
+        output  logic [31:0]    PC     
+    );
+    
+    
+endmodule
